73e554a0c919df09dd25588c50852f8101064377
Ic3Axe <93092619+Ic3Axe@users.noreply.github.com>
2022-10-14 10:25:18 +0200

    Add files via upload

--- Labb2.py
+++ Labb2.py
@@ -67,34 +67,68 @@ def main():
 main()
 
 """
+import random
+
 yard_meter = float(0.9144)
-foot_meter = float(0.3048)
+feet_meter = float(0.3048)
 inch_meter = float(0.0254)
 #Funktion som sätter värdet på faktorn beroende på vilken enhet som användaren vill ha.
 def printtable():
 	print("=============")
 	print("Table of " + chosen_unit)
 	print("=============")
+
 	if chosen_unit == "yard":
 		unit_meter = yard_meter
 
-	elif chosen_unit == foot_meter:
-		unit_meter = foot_meter
+	elif chosen_unit == "feet":
+		unit_meter = feet_meter
 	else:
 		unit_meter = inch_meter
+	
 	#printar tabellen
 	for t in range(1, i+1):
-		print(unit_meter*t)
+		print(unit_meter*t, chosen_unit)
 
-unit_list = ["yard", "foot", "inch"] 
-while True:
-	#Frågar användaren vilken längdenhet och hur många produkter hen vill ha i tabellen.
-	chosen_unit = str(input("Would you like to see the table for yard, foot or inch?: "))
-	i =	int(input("How many products do you want?"))
-	#kollar om enheten finns i listan
-	if chosen_unit.lower() in unit_list:
-		printtable()
+unit_list = ["yard", "feet", "inch"] 
+
+def unit_table():
+	while True:
+		#Frågar användaren vilken längdenhet och hur många produkter hen vill ha i tabellen.
+		chosen_unit = str(input("Would you like to see the table for yard, feet or inch?: "))
+		i =	int(input("How many products do you want?: "))
+		#kollar om enheten finns i listan
+		if chosen_unit.lower() in unit_list:
+			printtable()
+		else:
+			print("Chosen unit not found.")
+
+
+def guessing_game():
+	chosen_unit = str(input("What unit would you like to guess? Yard, feet or inch?: "))
+	if chosen_unit == "yard":
+		unit_meter = yard_meter
+
+	elif chosen_unit == "feet":
+		unit_meter = feet_meter
 	else:
-		print("Chosen unit not found.")
-
-printtable()
+		unit_meter = inch_meter
+	tries = 1
+	i = random.randint(0, 10)
+	while True:
+		print("What is", i,"meters in", chosen_unit, "?: ")
+		guess = float(input())
+		if guess < i*0.9*unit_meter or guess > i*1.1*unit_meter:
+			print("Nope, try again!")
+			tries += 1
+		else:
+			print("Right answer! It took", tries, "tries!")
+			break 
+while True:
+	program = str(input("Would you like to see the unit table or the guessing game?: "))
+	if program.lower() == "unit table":
+		unit_table()
+	elif program.lower() == "guessing game": 
+		guessing_game()
+	else:
+		print("Please enter guessing game or unit table")
